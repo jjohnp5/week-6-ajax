2fd7dfe02719f3736a9d39465150b9e0585a5068
Lagmay <quickfire@uspicorp.com>
2018-03-07 07:41:39 -0600

    updated some js keys

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
 
 $(document).ready(()=>{
-    $(document).on('click', '.button', clickHandler);
+    const API_Key = "XdGBrwP4R5Z83xSbFkfOdK9AautR3j4P"
+    const baseURL = "https://api.giphy.com/v1/gifs/search?api_key="+API_Key+"&";
+
+    $(document).on('click', '.searches', clickHandler);
 
     $('#submit').on('click', (e)=>{
         e.preventDefault();
-        $('.buttons').append($('<button>').data("id", $("#search").val()).text($("#search").val()).addClass("btn btn-success btn-sm searches"));
+        const val = $("#search").val();
+        $('.buttons').append($(`<button data-name="${val}">`).text(val).addClass("btn btn-success btn-sm searches"));
     })
 
 
 
     function clickHandler(e){
         e.preventDefault();
-        alert('hander working');
+        const ratings = ["Y", "G", "PG", "PG-13", "R"];
+        const queryURL = 
     }
 
 
